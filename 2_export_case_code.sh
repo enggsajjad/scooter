51996ef551a43e37b2600777f217c0b64b70ad65
enggsajjad <enggsajjadhussain@gmail.com>
2022-03-29 01:17:16 +0200

    test EDIT 2ND

--- 2_export_case_code.sh
+++ 2_export_case_code.sh
@@ -6,6 +6,9 @@ ACUITY_PATH=../bin/
 export_ovxlib=${ACUITY_PATH}ovxgenerator
 
 $export_ovxlib \
+    --model-input ${NAME}.json \
+    --data-input ${NAME}.data \
+    --model-quantize ${NAME}.quantize \
     --reorder-channel '0 1 2' \
     --channel-mean-value '128 128 128 128' \
     --export-dtype quantized \
